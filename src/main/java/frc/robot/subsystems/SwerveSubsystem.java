5ac7c58e6e621ef117e4667d8b9dd0db32bffc39
11Edna <97411074+11Edna@users.noreply.github.com>
2024-01-15 14:57:29 -0600

    Not Deprecated, junipter working

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -6,12 +6,18 @@ import com.ctre.phoenix.sensors.WPI_PigeonIMU;
 
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StructArrayPublisher;
+import edu.wpi.first.networktables.StructPublisher;
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -68,9 +74,9 @@ public class SwerveSubsystem extends SubsystemBase {
             DriveConstants.kBackRightDriveAbsoluteEncoderOffsetRad,
             DriveConstants.kBackRightDriveAbsoluteEncoderReversed);
 
+
     private final WPI_PigeonIMU gyro = new WPI_PigeonIMU(13);
-    // private final SwerveDriveOdometry odometer = new SwerveDriveOdometry(DriveConstants.kDriveKinematics,
-    //        new Rotation2d(0), new SwerveModulePosition[] { frontLeft.getDrivePosition(), frontRight.getDrivePosition(), backLeft.getDrivePosition(), backRight.getDrivePosition()});
+
     SwerveDriveOdometry odometer = new SwerveDriveOdometry(
         DriveConstants.kDriveKinematics, gyro.getRotation2d(),
         new SwerveModulePosition[] {
@@ -89,7 +95,7 @@ public class SwerveSubsystem extends SubsystemBase {
             }
         }).start();
     }
-
+    
     public void zeroHeading() {
         gyro.reset();
     }
@@ -112,23 +118,6 @@ public class SwerveSubsystem extends SubsystemBase {
         Translation2d position = pose.getTranslation();
         return odometer.getPoseMeters();
       }
-    
-      
-    //   public Command toPose(Pose2d initial, Pose2d destination, Supplier<Pose2d> current) {
-    //     ArrayList<PathPoint> points = new ArrayList<>();
-    //     points.add(new PathPoint(initial.getTranslation(), initial.getRotation()));    
-    //     points.add(new PathPoint(destination.getTranslation(), destination.getRotation()));
-        
-    //     return new PPSwerveControllerCommand(
-    //         PathPlanner.generatePath(new PathConstraints(DriveConstants.kTeleDriveMaxSpeedMetersPerSecond, DriveConstants.kTeleDriveMaxAccelerationUnitsPerSecond), points),
-    //         current,
-    //         new PIDController(0.0, 0.0, 0.0),
-    //         new PIDController(0.0, 0.0, 0.0),
-    //         //new PIDController(ModuleConstants.kTurnPID[0], ModuleConstants.kTurnPID[1], ModuleConstants.kTurnPID[2]),
-    //         new PIDController(0.0, 0.0, 0.0),
-    //         this::setModuleStates);
-  //}
-
 
     public void resetOdometry(Pose2d pose) {
         odometer.resetPosition(gyro.getRotation2d(), new SwerveModulePosition[] {
@@ -148,6 +137,7 @@ public class SwerveSubsystem extends SubsystemBase {
         });
     }
 
+
     @Override
     public void periodic() {
         updateOdometery();
@@ -165,6 +155,7 @@ public class SwerveSubsystem extends SubsystemBase {
         SmartDashboard.putNumber("Encoder position BL", backLeft.getTurningPosition());
         SmartDashboard.putNumber("Encoder position BR", backRight.getTurningPosition());
     }
+    
 
     public void stopModules() {
         frontLeft.stop();
